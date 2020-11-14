49925a514638db3c8f5772cd8a3157a2f0b75fdd
Schneiderrrr <73019395+Schneiderrrr@users.noreply.github.com>
2020-11-14 14:41:13 +0500

    Создала класс хранилще, класс кругов. Круги рисуются.

--- LABA6/Form1.Designer.cs
+++ LABA6/Form1.Designer.cs
@@ -28,13 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Drawing = new System.Windows.Forms.Panel();
+            this.Insrtument = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.Drawing.SuspendLayout();
+            this.Insrtument.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Drawing
+            // 
+            this.Drawing.BackColor = System.Drawing.Color.White;
+            this.Drawing.Controls.Add(this.Insrtument);
+            this.Drawing.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Drawing.Location = new System.Drawing.Point(0, 0);
+            this.Drawing.Name = "Drawing";
+            this.Drawing.Size = new System.Drawing.Size(1020, 741);
+            this.Drawing.TabIndex = 0;
+            this.Drawing.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.Drawing.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Drawing_MouseDown);
+            // 
+            // Insrtument
+            // 
+            this.Insrtument.BackColor = System.Drawing.Color.Gray;
+            this.Insrtument.Controls.Add(this.pictureBox4);
+            this.Insrtument.Controls.Add(this.pictureBox3);
+            this.Insrtument.Controls.Add(this.pictureBox2);
+            this.Insrtument.Controls.Add(this.pictureBox1);
+            this.Insrtument.Location = new System.Drawing.Point(12, 12);
+            this.Insrtument.Name = "Insrtument";
+            this.Insrtument.Size = new System.Drawing.Size(125, 717);
+            this.Insrtument.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox2.Location = new System.Drawing.Point(34, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Fuchsia;
+            this.pictureBox3.Location = new System.Drawing.Point(65, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Aqua;
+            this.pictureBox4.Location = new System.Drawing.Point(96, 3);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1020, 741);
+            this.Controls.Add(this.Drawing);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Drawing.ResumeLayout(false);
+            this.Insrtument.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Drawing;
+        private System.Windows.Forms.Panel Insrtument;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- LABA6/Form1.cs
+++ LABA6/Form1.cs
@@ -12,9 +12,96 @@ namespace LABA6
 {
     public partial class Form1 : Form
     {
+        public Graphics gra;
+        public Pen pen;
+        public SolidBrush fig;
         public Form1()
         {
             InitializeComponent();
+            gra = Drawing.CreateGraphics();
+        }
+
+
+        public class CCircle
+        {
+            public int x, y, rad;
+            private int xc, yc;
+            public Pen pen;
+            public SolidBrush fig;
+            public CCircle()
+            {
+                this.x = Cursor.Position.X;
+                this.y = Cursor.Position.Y;
+                this.rad = 200;
+                pen = new Pen(Color.Black, 5);
+                fig = new SolidBrush(Color.White);
+            }
+
+            public void DrawCircle(Graphics g)
+            {
+                this.xc = this.x - rad;
+                this.yc = this.y - rad;
+                g.FillEllipse(this.fig, this.xc, this.yc, this.rad, this.rad);
+                g.DrawEllipse(this.pen, this.xc, this.yc, this.rad, this.rad);
+            }
+
+            ~CCircle()
+            {
+
+            }
+        }
+
+        class Storage : CCircle
+        {
+            private int size = 0, flag = 0;
+            private const int maxsize = 1000;
+            private CCircle[]arr;
+
+            public Storage()
+            {
+                arr = new CCircle[maxsize];
+            }
+            public void add(CCircle a)
+            {
+                this.arr[this.size] = a;
+                this.size++;
+            }
+            public void del(CCircle a)
+            {
+                for (int i = 0; i < this.size; i++)
+                    if (this.arr[i] == a)
+                    {
+                        this.arr[i] = null;
+                        this.size--;
+                    }
+            }
+            public CCircle get (CCircle a)
+            {
+                for (int i = 0; i<this.size; i++)
+                    if (this.arr[i] == a)
+                        this.flag = 1;
+
+                if (this.flag == 0)
+                    return null;
+                else return a;
+            }
+
+            ~Storage()
+            {
+                this.arr = null;
+            }
+        }
+        Storage store = new Storage();
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //PictureBox p = (PictureBox)sender;
+        }
+
+        private void Drawing_MouseDown(object sender, MouseEventArgs e)
+        {
+            CCircle circle = new CCircle();
+            circle.DrawCircle(gra);
+            store.add(circle);
         }
     }
 }
